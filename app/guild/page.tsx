d72471c00fdb711c689d7eab3c8ab849adf65dad
com6ao <61026397+com6ao@users.noreply.github.com>
2025-08-16 11:31:06 -0300

    Create page.tsx

---
+++ app/guild/page.tsx
@@ -0,0 +1,3 @@
+import type { Metadata } from "next";
+export const metadata: Metadata = { title: "Organização (Guilda) — enewRPG" };
+export default function Page() { return <h1>Organização (Guilda)</h1>; }
