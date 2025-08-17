1808714574387111582ce2ef146887ce0fbc3512
com6ao <61026397+com6ao@users.noreply.github.com>
2025-08-17 14:44:18 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+// /lib/supabase.ts
+"use client";
+import { createBrowserClient } from "@supabase/ssr";
 
-export const supabase = createClient(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
