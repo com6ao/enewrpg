97486b66dfce69e655a5f107376e0c6b20103d15
com6ao <61026397+com6ao@users.noreply.github.com>
2025-08-26 14:31:34 -0300

    Create character.ts

---
+++ types/character.ts
@@ -0,0 +1,17 @@
+export interface Character {
+  id: string;
+  name: string;
+  surname: string;
+  level?: number;
+  lvl?: number;
+  xp: number;
+  str?: number;
+  dex?: number;
+  intt?: number;
+  wis?: number;
+  cha?: number;
+  con?: number;
+  luck?: number;
+  universe?: string;
+  energy?: string;
+}
