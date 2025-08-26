2525ae6c9f27e0544a2b3bffaad3ea66469bd6a6
com6ao <61026397+com6ao@users.noreply.github.com>
2025-08-26 15:37:31 -0300

    Update character.ts

--- types/character.ts
+++ types/character.ts
@@ -2,16 +2,16 @@ export interface Character {
   id: string;
   name: string;
   surname: string;
-  level?: number;
+  level: number;
   lvl?: number;
   xp: number;
-  str?: number;
-  dex?: number;
-  intt?: number;
-  wis?: number;
-  cha?: number;
-  con?: number;
-  luck?: number;
+  str: number;
+  dex: number;
+  intt: number;
+  wis: number;
+  cha: number;
+  con: number;
+  luck: number;
   universe?: string;
   energy?: string;
 }
