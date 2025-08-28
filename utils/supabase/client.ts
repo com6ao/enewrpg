df1d3193e888fc86e586d51a16dfa3db69b43b89
com6ao <61026397+com6ao@users.noreply.github.com>
2025-08-28 16:23:27 -0300

    Create client.ts

---
+++ utils/supabase/client.ts
@@ -0,0 +1,8 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export function createClient() {
+  return createBrowserClient(url, key);
+}
