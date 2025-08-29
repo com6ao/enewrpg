87ded64e332a7dac8245f48c68c1709287162a50
com6ao <61026397+com6ao@users.noreply.github.com>
2025-08-29 10:46:57 -0300

    Delete utils/supabase directory

--- utils/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export function createClient() {
-  return createBrowserClient(url, key);
-}
